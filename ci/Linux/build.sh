bf6c13b8cdd391a4448b34dd11d2f77775d61da1
michaelrossherron <michaelrossherron@gmail.com>
2025-08-01 12:20:24 -0600

    Updated Linux build script

--- ci/Linux/build.sh
+++ ci/Linux/build.sh
@@ -8,7 +8,25 @@ main() {
     git clone --depth 1 -b febiostudio3 https://github.com/febiosoftware/FEBioStudio.git
     ln -s febio-sdk FEBioStudio/febio-sdk
 	pushd FEBioStudio
-    ./ci/Linux/build.sh
+
+    QT_DIR="/opt/Qt/6.7.3/gcc_64"
+    cmake -L . -B cmbuild \
+        -DQt_Root=$QT_DIR \
+        -DFEBio_SDK=febio4-sdk \
+        -DUSE_FFMPEG=ON \
+        -DUSE_TETGEN=ON \
+        -DUSE_MMG=ON \
+        -DUSE_SSH=ON \
+        -DUSE_SSL=ON \
+        -DCAD_FEATURES=ON \
+        -DUSE_NETGEN=ON \
+        -DUSE_ITK=ON \
+        -DUSE_PYTHON=ON \
+        -DBUILD_UPDATER=ON
+
+    pushd cmbuild
+    make FSCore GLLib GLWLib OGLLib ImageLib CUILib -j $(nproc) 
+    popd
     popd
     
 	run_cmake
